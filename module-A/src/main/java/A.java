f7d6039774740599bfcc38cbaadfa3cfef093af7
Alexander Floh <alexander.floh@gmail.com>
2017-01-24 08:56:35 +0100

    Updated project with changes from launch4j

--- module-A/src/main/java/A.java
+++ module-A/src/main/java/A.java
@@ -3,5 +3,11 @@ public class A {
     public static void main(String[] args) {
         B b = new B();
         System.out.println("Hello from A!");
+        if(A.class.getClassLoader().getResource("someConfigFile.xml") == null) {
+            System.out.println("file not found on classpath");
+        }
+        else {
+            System.out.println("file found!");
+        }
     }
 }
